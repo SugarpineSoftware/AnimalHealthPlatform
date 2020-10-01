0239cea19cf15e88bf477749ae3acff71ae89de3
SugarpineSoftware <corygreen84@hotmail.com>
2020-10-01 12:16:43 -0700

    Implementing Vuex
    Implementing Vuex in the project

--- vuefrontend/src/main.js
+++ vuefrontend/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+
+// import {store} from './Store'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuex);
 
 new Vue({
   render: h => h(App),
